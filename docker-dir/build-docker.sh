27e3842ad95c6a0d769cf430f7af49c81f25575c
vidmantas zemleris <vidma@kensu.io>
2021-10-22 16:02:28 +0300

    use published docker

--- docker-dir/build-docker.sh
+++ docker-dir/build-docker.sh
@@ -12,3 +12,8 @@ docker tag bd2d11a00d7b vidma/wildfly-ksu
 #  => => writing image sha256:71dddc7a163150e7016bdecc834fbf073fb4028639b95a83414ce41cf16912d9                                                                    0.0s
 
 #p.s. this dont work easily: docker build - < Dockerfile
+
+docker tag bd2d11a00d7b vidmasze/ksu-wildfly:ksu-wildfly-setup
+docker push vidmasze/ksu-wildfly:ksu-wildfly-setup
+
+# ksu-wildfly-setup: digest: sha256:2077404858b2f044946884e3ebfe94cd7ea8d799a4bc127c41ad256395b0de3a size: 1789
